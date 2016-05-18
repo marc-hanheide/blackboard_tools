41802957e14e8a4dd8ada9fce82e439c9be4ccb0
Marc Hanheide <marc@hanheide.de>
2016-05-18 10:23:52 +0200

    added first PDDL test

--- pddl_test.py
+++ pddl_test.py
@@ -1,6 +1,10 @@
 import requests
 from pprint import pprint
 from re import match
+import argparse
+from glob import glob
+import csv
+
 
 def plan(domain_file, problem_file):
     url_prefix = "http://lcas.lincoln.ac.uk/fast-downward/"
@@ -21,7 +25,7 @@ def plan(domain_file, problem_file):
     actions = []
     if len(plan) > 1:
         for action in plan:
-            m = match(r'[(]([a-z]*) .*[)]', action)
+            m = match('[(]([a-z,_]*) .*[)]', action)
             if m:
                 actions.append(m.group(1))
 
@@ -35,4 +39,49 @@ def plan(domain_file, problem_file):
     return result
 
 
-pprint(plan('domain.txt', 'problem.txt'))
+parser = argparse.ArgumentParser(description='compute similarity')
+parser.add_argument('glob',
+                    help='path glob pattern')
+
+parser.add_argument('--problem',
+                    help='list of problem files',
+                    action='append')
+
+parser.add_argument('--csv_file',
+                    help='result csv file', default='pddl-results.csv')
+
+
+
+args = parser.parse_args()
+
+files = glob(args.glob)
+
+with open(args.csv_file, "w") as csv_file:
+    csv_writer = csv.writer(csv_file, dialect='excel')
+
+    csv_row = ['domain_file']
+    for p in args.problem:
+        csv_row.append(p + ' plan')
+        csv_row.append(p + ' sout')
+        csv_row.append(p + ' plan length')
+    csv_writer.writerow(csv_row)
+
+    for domain_file in files:
+        print domain_file
+        csv_row = [domain_file]
+        for problem_file in args.problem:
+            try:
+                p = plan(domain_file, problem_file)
+                #pprint(p)
+                csv_row.append(' '.join(p['actions']))
+                if p['solution_found']:
+                    csv_row.append('TRUE')
+                else:
+                    csv_row.append(p['sout'])
+                csv_row.append(p['length'])
+            except Exception as e:
+                print "  ", e
+                csv_row.append(str(e))
+                csv_row.append('')
+                csv_row.append('')
+        csv_writer.writerow(csv_row)
---
+++ wolf-goat-cabbage-problem-empty.pddl
@@ -0,0 +1,26 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; problem definition for wolf, goat, cabbage problem
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define 
+	(problem boat1)
+	(:domain boat)
+    ; only needs two objects, namely representing
+    ; either banke side of the river, [w]est and [e]ast
+	(:objects  w e)
+	(:INIT 
+     	; wolf, goat, cabbage, boat are all on 
+     	; the west side to start with
+        (config w w w w)
+
+    )
+  
+	(:goal (AND 
+        	; they all have to move to the east side
+			(config w w w w)
+		)
+	)
+)
+
+
+
